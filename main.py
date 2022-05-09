33fffd5f60f18375f6e018a15fb74b4ded73e531
Monil Darediya <103734757+MonilDared@users.noreply.github.com>
2022-05-09 16:01:21 +0000

    Main File

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import os
+import pytz
 import datetime
 os.system("clear")
 print("ＥａｓｙＷｅｂＦｒｏｍＧｉｔｈｕｂ\n----------------------------------")
-time = datetime.datetime.now(datetime.timezone).strftime("%a--%b--%Y\n%-Ih--%-Mm--%p")
-notn,yes = os.getcwd().split('/home/runner/')
+
 loop = True
 while loop:
+  time = datetime.datetime.now(pytz.timezone('Asia/Calcutta')).strftime("%a--%b--%Y\n%-Ih--%-Mm--%p")
+  notn,yes = os.getcwd().split('/home/runner/')
   inputa = input(f"{time}\n{yes} $ ")
   if inputa.startswith("code "):
     notne,ne = inputa.split("code ")
     print(f"<code>{ne}</code>")
-  if inputa == "exit":
+  elif inputa == "exit":
     loop = False
-  if inputa == "cla":
+  elif inputa == "cla":
     os.system("clear")
     print("ＥａｓｙＷｅｂＦｒｏｍＧｉｔｈｕｂ\n----------------------------------")
+  elif inputa.startswith("heading "):
+    notne,ne = inputa.split("heading ")
+    print(f"<h1>{ne}</h1>")
