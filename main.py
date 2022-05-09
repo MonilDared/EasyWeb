b56878ce58be0e9d7926fa7a19a885cecabea2c7
Monil Darediya <103734757+MonilDared@users.noreply.github.com>
2022-05-09 06:43:22 +0000

    Main File

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ import os
 import datetime
 os.system("clear")
 print("ＥａｓｙＷｅｂＦｒｏｍＧｉｔｈｕｂ\n----------------------------------")
-time = datetime.datetime.now().strftime("%a--%b--%Y\n%-Ih--%-Mm--%p")
+time = datetime.datetime.now(datetime.timezone).strftime("%a--%b--%Y\n%-Ih--%-Mm--%p")
 notn,yes = os.getcwd().split('/home/runner/')
-inputa = input(f"{time}\n{yes} $ ")
-if inputa.startswith("code "):
-  notne,ne = inputa.split("code ")
-  print(f"<code>{ne}</code>")
+loop = True
+while loop:
+  inputa = input(f"{time}\n{yes} $ ")
+  if inputa.startswith("code "):
+    notne,ne = inputa.split("code ")
+    print(f"<code>{ne}</code>")
+  if inputa == "exit":
+    loop = False
+  if inputa == "cla":
+    os.system("clear")
+    print("ＥａｓｙＷｅｂＦｒｏｍＧｉｔｈｕｂ\n----------------------------------")
